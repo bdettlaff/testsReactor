f270ed0b28e3b5a2736d12f35ebe1933b2208965
dettlaffb <209295@edu.p.lodz.pl>
2019-05-15 17:34:16 +0200

    SetUp for future Tests

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc2/WashingMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc2/WashingMachineTest.java
@@ -3,9 +3,25 @@ package edu.iis.mto.testreactor.exc2;
 import static org.junit.Assert.assertThat;
 
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class WashingMachineTest {
+    private WaterPump waterPump;
+    private DirtDetector dirtDetector;
+    private Engine engine;
+
+    private WashingMachine washingMachine;
+
+    @Before
+    public void setUp(){
+        waterPump = Mockito.mock(WaterPump.class);
+        dirtDetector = Mockito.mock(DirtDetector.class);
+        engine = Mockito.mock(Engine.class);
+
+        washingMachine = new WashingMachine(dirtDetector,engine,waterPump);
+    }
 
     @Test
     public void itCompiles() {
